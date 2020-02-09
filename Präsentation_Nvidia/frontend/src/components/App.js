1d7255e34b6018a9db81e64ce09baee97b5621c1
PhilippNiklas <philipp-doebler1502@gmx.de>
2020-02-09 13:56:46 +0100

    finshed adding react

--- Präsentation_Nvidia/frontend/src/components/App.js
+++ Präsentation_Nvidia/frontend/src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component} from 'react'
 import ReactDOM from 'react-dom'
+import Header from './layout/Header'
 
 class App extends Component {
     render() {
-        return <h1>Präsentation</h1>
+        return (
+            <Header />
+        )
     }
 }
 
